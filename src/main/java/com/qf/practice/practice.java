edf2df0d6a3f0706481d284be81e4352f15da47a
wangxiong <1956018460@qq.com>
2020-04-29 00:08:13 +0800

    this is laowang first commit,please check it

---
+++ src/main/java/com/qf/practice/practice.java
@@ -0,0 +1,56 @@
+package com.qf.practice;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author Wang
+ * @Date 2020/4/23
+ */
+
+public class practice {
+//    public static void main(String[] args) {
+//        float a=2.0f - 0.9f;
+//        System.out.println(a);
+//        float b=1.9f - 0.8f;
+//        System.out.println(b);
+//        if(a==b){
+//            System.out.println("true");
+//        }else{
+//            System.out.println("false");
+//        }
+//
+//        String aa=null;
+//        String bb="abc";
+//        String cc="";
+//        System.out.println(aa+bb);
+//        System.out.println(aa+cc);
+//        System.out.println(bb+cc);
+//    }
+
+    public static void main(String[] args) {
+//        BigDecimal b=new BigDecimal(0.1);
+//        System.out.println(b);
+//        BigDecimal c=new BigDecimal(0.1);
+//        System.out.println(c);
+//        System.out.println(b==c);
+//        List<BigDecimal> list=new ArrayList<>();
+//        for (int i = 0; i < 2; i++) {
+//            list.add(new BigDecimal(i));
+//        }
+//        list.forEach(val->{
+//           val=val.add(BigDecimal.ONE);
+//            System.out.println(val);
+//        });
+//        System.out.println(BigDecimal.ONE);
+       String str1="a"+"c";
+       String str2=new String(str1);
+       if(str2=="ac")
+           System.out.println("==");
+        if(str2.equals("ac"))
+            System.out.println("dfhkj");
+
+    }
+
+}
